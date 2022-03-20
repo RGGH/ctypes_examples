88f4c6d6bff667c89c9050519e3baf59417f7e67
RGGH <pisscompany@gmail.com>
2022-03-20 17:50:39 +0000

    c2

--- basic_function_helper.py
+++ basic_function_helper.py
@@ -9,6 +9,8 @@ from ctypes import c_double, c_int, CDLL
 
 import sys
 
+import timeit
+
 
 lib_path = "tractor/basic_function_%s.so" % (sys.platform)
 
@@ -30,12 +32,11 @@ def do_square_using_c(list_in):
 
     n = len(list_in)
 
-
-    c_arr_in = (c_double *n)(*list_in)
-    # ctype array of double of size n that can be used by the C function. 
+    c_arr_in = (c_double * n)(*list_in)
+    # ctype array of double of size n that can be used by the C function.
     # initialised with the values of list_in.
 
-    c_arr_out = (c_double *n)()
+    c_arr_out = (c_double * n)()
 
     python_c_square(c_int(n), c_arr_in, c_arr_out)
 
@@ -43,3 +44,25 @@ def do_square_using_c(list_in):
 
 
 print(f"{do_square_using_c([3,6])} \n")
+
+
+start_time = timeit.default_timer()
+
+do_square_using_c([3, 6])
+time_c = timeit.default_timer() - start_time
+print("The C time difference is :", time_c)
+
+
+# Compare versus using a python function #
+
+
+def p_square(lst):
+    for i in lst:
+        i = i * i
+        print(str(i))
+
+
+start_time = timeit.default_timer()
+p_square([3, 6])
+time_p = timeit.default_timer() - start_time
+print("The Python time difference is :", time_p)
